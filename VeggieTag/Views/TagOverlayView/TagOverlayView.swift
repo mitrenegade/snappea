a71062d815ec6af3acf657dad9ad8b69be6f1591
Bobby Ren <bobbyren@gmail.com>
2020-04-25 14:48:43 -0700

    moved TagView; added veggie photos

--- VeggieTag/Views/TagOverlayView/TagOverlayView.swift
+++ VeggieTag/Views/TagOverlayView/TagOverlayView.swift
@@ -22,9 +22,18 @@ struct TagOverlayView: View {
             AsyncImageView(url: url,
                            placeholder: Text("Loading..."))
                 .aspectRatio(contentMode: .fit)
+                .border(Color.black)
+                .frame(width: 250, height: 250)
             ForEach(tags) {tag in
                 TagView(tag: tag)
             }
         }
     }
 }
+
+struct TagOverlayView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagOverlayView(url: URL(string: "https://i.redd.it/gbxhi6mwdvt41.jpg")!, tags: tagData)
+    }
+}
+
