41718a090422595ecd5ff987a0afa5c99d110c2f
Bobby Ren <bobbyren@gmail.com>
2020-04-25 14:56:31 -0700

    made imageStore into a class

--- VeggieTag/Services/ImageStore.swift
+++ VeggieTag/Services/ImageStore.swift
@@ -1,39 +1,14 @@
-/*
-See LICENSE folder for this sample’s licensing information.
+//
+//  ImageStore.swift
+//  VeggieTag
+//
+//  Created by Bobby Ren on 4/25/20.
+//  Copyright © 2020 RenderApps LLC. All rights reserved.
+//
 
-Abstract:
-Helpers for loading images and data.
-*/
-
-import UIKit
 import SwiftUI
-import CoreLocation
-
-let plantData: [Plant] = load("plantData.json")
-let tagData: [Tag] = load("tagData.json")
-let photoData: [Photo] = load("photoData.json")
-
-func load<T: Decodable>(_ filename: String) -> T {
-    let data: Data
-    
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-        else {
-            fatalError("Couldn't find \(filename) in main bundle.")
-    }
-    
-    do {
-        data = try Data(contentsOf: file)
-    } catch {
-        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
-    }
-    
-    do {
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    } catch {
-        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
-    }
-}
+import Combine
+import Foundation
 
 final class ImageStore {
     typealias _ImageDictionary = [String: CGImage]
@@ -67,4 +42,3 @@ final class ImageStore {
         return images.index(forKey: name)!
     }
 }
-
