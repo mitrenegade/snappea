63df7b8c86c60e6833a53a105a3449b1abfa296d
Bobby Ren <bobbyren@gmail.com>
2020-04-26 10:52:05 -0700

    Cleanup

--- VeggieTag/ViewModels/PhotoDetailViewModel.swift
+++ VeggieTag/ViewModels/PhotoDetailViewModel.swift
@@ -12,17 +12,10 @@ import Foundation
 class PhotoDetailViewModel: ObservableObject {
     @Published var photo: Photo
     @Published var tags = [Tag]()
-    var urlString: String = ""
     private var cancellables = Set<AnyCancellable>()
     
     init(photo: Photo) {
         self.photo = photo
-        
-        // assign url
-        $photo
-            .map{ $0.url }
-            .assign(to: \.urlString, on: self)
-            .store(in: &cancellables)
         
         tags = tagData
             .filter{ $0.photoId == photo.id }
