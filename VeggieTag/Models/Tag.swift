fe8b212b659ea45887ae9f02e0fa5558dfba96f5
Bobby Ren <bobbyren@gmail.com>
2020-05-06 20:38:30 -0700

    Tag has initializer

--- VeggieTag/Models/Tag.swift
+++ VeggieTag/Models/Tag.swift
@@ -9,29 +9,28 @@
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-protocol Taggable
-{
-    var x: CGFloat { get }
-    var y: CGFloat { get }
-    var scale: CGFloat? { get }
-}
-
-struct Tag: Identifiable, Codable, Taggable {
-    var scale: CGFloat? = 1
-
+struct Tag: Identifiable, Codable {
     @DocumentID var id: String? = nil
     var photoId: String = ""
     var plantId: String = ""
-    var x: CGFloat = 0
-    var y: CGFloat = 0
-//    @ServerTimestamp var createdTime: Timestamp?
+    var x0: CGFloat
+    var y0: CGFloat
+    var x1: CGFloat? = nil
+    var y1: CGFloat? = nil
     
     var plant: Plant? {
         return APIService.shared.plants.first { $0.id == plantId }
-
     }
     
     var photo: Photo? {
         return APIService.shared.photos.first { $0.id == photoId }
+    }
+    
+    init(photoId: String, x0: CGFloat, y0: CGFloat, x1: CGFloat? = nil, y1: CGFloat? = nil) {
+        self.photoId = photoId
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y1 = y1
     }
 }
