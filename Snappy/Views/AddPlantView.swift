cc2d41cc51d89814231ba0df6333cdcf859b78aa
Bobby Ren <bobbyren@gmail.com>
2023-12-28 22:48:25 -0800

    wip added pickers for category and type

--- Snappy/Views/AddPlantView.swift
+++ Snappy/Views/AddPlantView.swift
@@ -9,21 +9,64 @@
 import SwiftUI
 
 struct AddPlantView: View {
+
+    @State var name: String = ""
+    @State var category: Category = .other
+    @State var plantType: PlantType = .unknown
+
     var body: some View {
         VStack {
-            HStack {
-                Button {
-                    // camera
-                } label: {
-                    Image(systemName: "camera")
-                }
-                .frame(width: 100, height: 100)
-                .aspectRatio(contentMode: .fill)
-                .foregroundColor(Color.black)
-                .background(Color.green)
-                .clipShape(Circle())
-            }
+            addButton
+            nameField
+            categoryField
+            typeField
         }
+    }
+
+    private var addButton: some View {
+        HStack {
+            Button {
+                // camera
+            } label: {
+                Image(systemName: "camera")
+            }
+            .frame(width: 100, height: 100)
+            .aspectRatio(contentMode: .fill)
+            .foregroundColor(Color.black)
+            .background(Color.green)
+            .clipShape(Circle())
+        }
+    }
+
+    private var nameField: some View {
+        HStack {
+            Spacer()
+            TextField(text: $name) {
+                Text("Plant name")
+            }
+            .border(.gray)
+            Spacer()
+        }
+    }
+
+    private var typeField: some View {
+            List {
+                Picker("Type", selection: $plantType) {
+                    ForEach(PlantType.allCases) { plantType in
+                        Text(plantType.rawValue.capitalized)
+                    }
+                }
+            }
+    }
+
+    private var categoryField: some View {
+            List {
+                Picker("Category", selection: $category) {
+                    ForEach(Category.allCases) { category in
+                        Text(category.rawValue.capitalized)
+                    }
+                }
+            }
     }
 }
 
