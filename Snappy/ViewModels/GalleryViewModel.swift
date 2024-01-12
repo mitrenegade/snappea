3f7311976e4f03ea23ddc48e652f91c72f922d7a
Bobby Ren <bobbyren@gmail.com>
2024-01-11 21:58:02 -0600

    Added GalleryRoot

---
+++ Snappy/ViewModels/GalleryViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  GalleryViewModel.swift
+//  Snappy
+//
+//  Created by Bobby Ren on 1/11/24.
+//  Copyright © 2024 RenderApps LLC. All rights reserved.
+//
+
+import Foundation
+
+class GalleryViewModel: ObservableObject {
+    @Published var dataSource: [Photo] = []
+    @Published var router: HomeViewRouter
+
+    init(apiService: APIService, dataStore: DataStore, router: HomeViewRouter) {
+        self.router = router
+
+        Task {
+            try await apiService.loadGarden()
+
+            dataSource = dataStore.allPhotos
+        }
+    }
+}
