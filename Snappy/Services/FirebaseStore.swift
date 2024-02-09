d1057d9e40488312b1d8a8bb078df66f2cde797b
Bobby Ren <bobbyren@gmail.com>
2024-02-08 20:56:09 -0800

    consolidate FirebaseAPIservice and FirebaseStore

--- Snappy/Services/FirebaseStore.swift
+++ Snappy/Services/FirebaseStore.swift
@@ -8,24 +8,35 @@
 
 import Foundation
 import UIKit
+import FirebaseFirestore
 
 /// An implementation of Store that uses Firebase's API, via FirebaseAPIService
 class FirebaseStore: Store {
-    private let api = FirebaseAPIService()
+
+    /// Auth
+    private let auth: AuthStore
+
+    /// Firebase Database
+    let db = Firestore.firestore()
+
+    // MARK: - Initialization
+    init(authStore: AuthStore = AuthStore.shared) {
+        self.auth = authStore
+    }
 
     func loadGarden() async throws {
         let group = DispatchGroup()
         Task {
             group.enter()
-            allPhotos = try await api.fetchPhotos()
+            allPhotos = try await fetchPhotos()
         }
         Task {
             group.enter()
-            allPlants = try await api.fetchPlants()
+            allPlants = try await fetchPlants()
         }
         Task {
             group.enter()
-            allSnaps = try await api.fetchSnaps()
+            allSnaps = try await fetchSnaps()
         }
         group.notify(queue: DispatchQueue.global()) {
             print("Load garden complete with \(self.allPhotos.count) photos, \(self.allPlants.count) plants, \(self.allSnaps.count) snaps")
@@ -71,14 +82,18 @@ class FirebaseStore: Store {
         let id = UUID().uuidString // TODO use firebase id
         let timestamp = Date().timeIntervalSince1970
         let photo = Photo(id: id, timestamp: timestamp)
-        api.addPhoto(photo) { result, error in
+        try addPhoto(photo) { result, error in
             guard var newPhoto = result else {
-              return
-          }
+                return
+            }
             FirebaseImageService.uploadImage(image: image, type: .photo, uid: photo.id) { [weak self] result in
                 if let url = result {
-                    self?.api.updatePhotoUrl(newPhoto, url: url) { error in
-                        newPhoto.url = url // manually update url in existing photo object locally
+                    do {
+                        try self?.updatePhotoUrl(newPhoto, url: url) { error in
+                            newPhoto.url = url // manually update url in existing photo object locally
+                        }
+                    } catch {
+                        print("Update photo \(error)")
                     }
                 }
             }
@@ -88,7 +103,6 @@ class FirebaseStore: Store {
     }
 
     func createPlant(name: String, type: PlantType, category: Category) throws {
-        // BR TODO
     }
 
     func createSnap(photo: Photo, start: CGPoint, end: CGPoint, imageSize: CGSize) throws -> Snap {
@@ -97,3 +111,85 @@ class FirebaseStore: Store {
     }
 }
 
+/// Direct calls to API
+extension FirebaseStore {
+    private var userId: String? {
+        auth.user?.id
+    }
+
+    // MARK: - API Interface
+    private func fetchPhotos() async throws -> [Photo] {
+        try await fetchObjects(collection: "photos")
+    }
+
+    private func fetchPlants() async throws -> [Plant] {
+        try await fetchObjects(collection: "plants")
+    }
+
+    private func fetchSnaps() async throws -> [Snap] {
+        try await fetchObjects(collection: "snaps")
+    }
+
+    // MARK: - Generic interface into Firebase
+    /// Fetches an array of an object type given a collection name
+    private func fetchObjects<T: Decodable>(collection: String) async throws -> [T] {
+        guard let userId = userId else {
+            throw StoreError.notAuthorized
+        }
+        return try await withCheckedThrowingContinuation { continuation in
+            db.collection(userId).document("garden").collection(collection).addSnapshotListener { (snapshot, error) in
+                guard let snapshot else {
+                    continuation.resume(throwing: StoreError.databaseError(error))
+                    return
+                }
+                let objects = snapshot.documents.compactMap { document -> T? in
+                    try? document.data(as: T.self)
+                }
+                continuation.resume(returning: objects)
+            }
+        }
+    }
+
+    // upload to db and save locally
+    private func add<T: Codable>(_ object: T,
+                                   collection: String,
+                                   completion: @escaping ((T?, Error?)->Void)) throws {
+        guard let userId = userId else {
+            throw StoreError.notAuthorized
+        }
+        do {
+            let ref = try db.collection(userId)
+                .document("garden").collection(collection)
+                .addDocument(from: object)
+            ref.getDocument { (snapshot, error) in
+                if let result = try? snapshot?.data(as: T.self) {
+                    completion(result, error)
+                }
+            }
+        } catch let error {
+            print("AddPhoto error \(error)")
+        }
+    }
+
+    private func updatePhotoUrl(_ photo: Photo, url: String, completion: ((Error?)->Void)? = nil) throws {
+        guard let userId = userId else {
+            throw StoreError.notAuthorized
+        }
+        let ref = db.collection(userId).document("garden").collection("photos").document(photo.id)
+        ref.updateData(["url":url], completion: completion)
+    }
+
+    // only creates Photo object; image upload and url update are done separately
+    private func addPhoto(_ photo: Photo, result: @escaping ((Photo?, Error?)->Void)) throws {
+        try add(photo, collection: "photos", completion: result)
+    }
+
+    private func addPlant(_ plant: Plant, result: @escaping ((Plant?, Error?)->Void)) throws {
+        try add(plant, collection: "plants", completion: result)
+    }
+
+    private func addSnap(_ snap: Snap, result: @escaping ((Snap?, Error?)->Void)) throws {
+        // TODO: also update plants and photos?
+        try add(snap, collection: "snaps", completion: result)
+    }
+}
