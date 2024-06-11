2ef1343684b5f94694dcdac3d4cd2bdb7df4ccff
Bobby Ren <bobbyren@gmail.com>
2024-06-11 09:57:36 -0400

    Added action sheet for sorting and searching

--- Snappy/Views/Plants/PlantsListView.swift
+++ Snappy/Views/Plants/PlantsListView.swift
@@ -13,12 +13,16 @@ struct PlantsListView<T>: View where T: Store {
     @ObservedObject var store: T
     @Binding var selectedPlant: Plant?
 
+    @State private var showingSheet = false
+    @State private var isSheetSearch = false
+
     var body: some View {
         VStack {
             HStack {
                 Spacer()
-                optionsButton.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
-            }
+                searchButton
+                sortButton
+            }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
             List(store.allPlants, selection: $selectedPlant) { plant in
                 let photo = store.photos(for: plant)
                     .sorted { $0.timestamp > $1.timestamp }
@@ -29,18 +33,53 @@ struct PlantsListView<T>: View where T: Store {
             }
             Spacer()
         }
+        .actionSheet(isPresented: $showingSheet) { () -> ActionSheet in
+            if isSheetSearch {
+                searchSheet
+            } else {
+                sortSheet
+            }
+        }
     }
 
-    private var optionsButton: some View {
+    private var searchButton: some View {
         Button {
-            showOptions()
+            showingSheet = true
+            isSheetSearch = true
+        } label: {
+            Image(systemName: "magnifyingglass")
+        }
+    }
+
+    private var sortButton: some View {
+        Button {
+            showingSheet = true
+            isSheetSearch = false
         } label: {
             Image(systemName: "ellipsis.circle")
         }
-
     }
 
-    private func showOptions() {
+    private var sortSheet: ActionSheet {
+        let buttons: [ActionSheet.Button] = [
+            .default(Text("Name"), action: {
+                //                self.sort()
+            }),
+            .default(Text("Category"), action: {
+                //                self.search()
+            }),
+            .cancel()
+        ]
+        return ActionSheet(title: Text("Sort plants by:"), message: nil, buttons: buttons)
+    }
 
+    private var searchSheet: ActionSheet {
+        let buttons: [ActionSheet.Button] = [
+            .default(Text("Search"), action: {
+                //                self.sort()
+            }),
+            .cancel()
+        ]
+        return ActionSheet(title: Text("Search"), message: nil, buttons: buttons)
     }
 }
